5788608e9345de189586f63dd246b5d8706a4b85
Alex C <seafoox@gmail.com>
2020-02-14 11:18:01 +0100

    Added login logic to 10 Authentification

--- exercices/10 Authentification/routes/login.js
+++ exercices/10 Authentification/routes/login.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const fs = require('fs');
 const router = express.Router();
 
 router.get('/', (req, res) => {
@@ -6,5 +7,38 @@ router.get('/', (req, res) => {
   	title: 'Login'
   });
 });
+
+router.post('/send', (req, res) => {
+  let resultMessage = '';
+  const form = req.body;
+
+  const userFound = verifyUser(form.login, form.password);
+
+  if (userFound) {
+    resultMessage = `Vous êtes connecté en temps que ${userFound.username}`;
+  }
+  else {
+    resultMessage = `L'identifiant ou le mot de passe est incorrect`;
+  }
+
+  res.render('login', {
+    title: 'Login',
+    info: resultMessage
+  });
+});
+
+function verifyUser(username, password) {
+  console.log(username, password);
+  const json = fs.readFileSync(__dirname + '/../users.json', 'utf8');
+  const usersList = JSON.parse(json);
+  let userFound = null;
+
+  userFound = usersList.find(user =>
+    user.username === username &&
+    user.password === password
+  );
+
+  return userFound;
+}
 
 module.exports = router;
