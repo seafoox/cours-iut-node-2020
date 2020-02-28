9dc08e9ea18bc2c31d1bfaa33988b73d71aafb7e
Alex C <seafoox@gmail.com>
2020-02-28 07:55:37 +0100

    Added correction for the session as part of the 10 - Authentification

--- exercices/10 Authentification/app.js
+++ exercices/10 Authentification/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const session = require('express-session');
 const defaultRoutes = require('./routes/default');
 const projectsRoutes = require('./routes/projects');
 const contactRoutes = require('./routes/contact');
@@ -9,8 +10,26 @@ const app = express();
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 
-// Ajout declaration moteur de template
+// Initialisaiton du moteur de template PUG
 app.set('view engine', 'pug');
+
+// Initialialisaiton & Configuration de la session via Cookie
+const sessionParams = {
+  secret: 'my_secret',
+  maxAge: 24 * 60 * 60 * 1000 // 24 hours
+};
+app.use(session(sessionParams));
+
+// Pour chaque nouvelle requette reçu, on regarde si une session
+// exist et contient userId. Si c'est le cas, alors recupère de cette
+// derniere le username, et on l'ajoute aux variable globales de vue.
+app.use((req, res, next) => {
+  if (req.session && req.session.userId) {
+    res.locals.username = req.session.username;
+  }
+  next();
+});
+
 
 app.use('/', defaultRoutes);
 app.use('/projects', projectsRoutes);
--- exercices/10 Authentification/routes/login.js
+++ exercices/10 Authentification/routes/login.js
@@ -4,23 +4,32 @@ const router = express.Router();
 
 router.get('/', (req, res) => {
   res.render('login', {
-  	title: 'Login'
+    title: 'Login'
   });
+});
+
+router.get('/logout', (req, res) => {
+  req.session.destroy();
+  res.redirect('/');
 });
 
 router.post('/send', (req, res) => {
   let resultMessage = '';
   const form = req.body;
 
-  const userFound = verifyUser(form.login, form.password);
+  const userFound = verifyUser(form.username, form.password);
 
+  // Si userFound !== null, on le sauvegarde dans la session à l'aide d'un cookie
   if (userFound) {
+    req.session.userId = userFound.id;
+    req.session.username = userFound.username;
+    res.locals.username = userFound.username;
+
     resultMessage = `Vous êtes connecté en temps que ${userFound.username}`;
   }
   else {
-    resultMessage = `L'identifiant ou le mot de passe est incorrect`;
+    resultMessage = 'L\'identifiant ou le mot de pass sont incorect';
   }
-
   res.render('login', {
     title: 'Login',
     info: resultMessage
@@ -28,17 +37,17 @@ router.post('/send', (req, res) => {
 });
 
 function verifyUser(username, password) {
-  console.log(username, password);
+  // Ouvre le fichier JSON avec la liste des users
   const json = fs.readFileSync(__dirname + '/../users.json', 'utf8');
-  const usersList = JSON.parse(json);
-  let userFound = null;
+  const users = JSON.parse(json);
 
-  userFound = usersList.find(user =>
+  // Vérification des infos reçu de la saisie du formulaire et comparaison avec notre users.json
+  let userFound = users.find(user =>
     user.username === username &&
     user.password === password
   );
 
-  return userFound;
+    return userFound;
 }
 
 module.exports = router;
