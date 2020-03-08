6a882c2f28abf6d26e3b95a795c22ee338d8a2b6
Alex C <seafoox@gmail.com>
2020-03-08 10:25:04 +0100

    Fixed files in ex13

--- exercices/13 Rest API/config.js
+++ exercices/13 Rest API/config.js
@@ -1,9 +1,9 @@
 module.exports = {
     database: {
-        user: "",
-        host: "",
-        database: "",
-        password: "",
+        user: "tbkoyffh",
+        host: "john.db.elephantsql.com",
+        database: "tbkoyffh",
+        password: "AgF-A5vaCFNapXo3w5foX-Rn7GmkeFHI",
         port: 5432
     },
     nodemailer: {}
